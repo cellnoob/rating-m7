5b6f03d0220fb32f4825b39d56c073a348cd1818
cellnoob <60471273+cellnoob@users.noreply.github.com>
2021-05-04 19:00:44 +0200

    Add files via upload
    #Cambios rating

--- myscript.js
+++ myscript.js
@@ -7,32 +7,32 @@
 */
 
 //Nivell 02
-  
+
 const container = document.querySelector(".rating");
 var items = container.querySelectorAll(".rating-items");
 var cuerpo = document.querySelector("body");
 
-    container.onclick = e =>{
-    const elClass = e.target.classList;
-    if (!elClass.contains('active')) {
-        items.forEach(
+container.onclick = (e) => {
+  const elClass = e.target.classList;
+  if (!elClass.contains("active")) {
+    items.forEach((item) => item.classList.remove("active"));
+    console.log(e.target.getAttribute("date-rate"));
+    document.getElementById("display").innerHTML = e.target.getAttribute(
+      "date-rate"
+    );
+    elClass.add("active");
+
+    cuerpo.onclick = (o) => {
+      console.log("Limpia resultado");
+      const elClassb = o.target.classList;
+      if (!elClassb.contains("active")) {
+        location.reload();
+        return false;
+        /*items.forEach(
             item => item.classList.remove('active')
         );
-        console.log(e.target.getAttribute("date-rate"));
-        document.getElementById("display").innerHTML = e.target.getAttribute("date-rate");
-        elClass.add('active');
-
-
-    cuerpo.onclick = o =>{
-    console.log("Limpia resultado");
-    const elClassb = o.target.classList;
-    if (!elClassb.contains("active")){
-        items.forEach(
-            item => item.classList.remove('active')
-        );
-        document.getElementById("display").innerHTML = "";
-    }
-    }
-}
+        document.getElementById("display").innerHTML = "";*/
+      }
+    };
+  }
 };
-
